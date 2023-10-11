a0798c9675fa799b9fa37520500d743534ede9c6
Merojxon <sirojiddinovmerojiddin66@gmail.com>
2023-10-11 18:16:38 +0500

    3-dars

---
+++ main.js
@@ -0,0 +1,102 @@
+// // for-1
+// let k = prompt("k sonini kiriting: ");
+// let n = prompt("n sonini kiriting");
+
+// for (let i=0; i!=n; i++){
+//     console.log(k)
+// }
+
+
+// // fo3-2
+// let a = parseInt(prompt('a sonini kirirting: '))
+// let b = prompt('b sonini kiriting: ')
+
+// for(; b>(a+1); --b){
+//         console.log(b-1)
+// }
+
+
+// // for4
+
+// let narx = prompt('narxni kiriting')
+
+// for(let i=1; i<=10; i++){
+//     console.log(i*narx)
+// }
+
+
+// // for5
+// let narx = prompt('narxni kiriting')
+
+// for(let i=0.1; i<=1; i+=0.1){
+//     console.log(i*narx)
+// }
+
+
+// // for6
+// let narx = prompt('narxni kiriting')
+
+// for(let i=0.2; i<=1; i+=0.2){
+//     console.log(i*narx)
+// }
+
+
+// // FOR7
+// let a = parseInt(prompt('a sonini kirirting: '))
+// let b = parseInt(prompt('b sonini kiriting: '))
+// let i=0
+// for(; b>=a; b--){
+//     i+=b
+// }
+// console.log(i);
+
+
+
+// // FOR8
+// let a = parseInt(prompt('a sonini kirirting: '))
+// let b = parseInt(prompt('b sonini kiriting: '))
+// let i=1
+// for(; b>=a; b--){
+//     i*=b
+// }
+// console.log(i);
+
+
+// // FOR9
+// let a = parseInt(prompt('a sonini kirirting: '))
+// let b = parseInt(prompt('b sonini kiriting: '))
+// let i=0
+// for(; b>=a; b--){
+//     i+=b**2
+// }
+// console.log(i);
+
+
+// //for10
+// let n = parseInt(prompt('n sonini kiriting: '))
+// let counter = 0
+
+// for(; n!=0; n--){
+// counter+= 1/n
+// }
+// console.log(counter);
+
+
+// //for11
+// let n = parseInt(prompt('n sonini kiriting: '))
+// let counter = 0
+
+// for(let i=n;n!=0; n--){
+//     counter+=(i+n)**2
+// }
+// console.log(counter);
+
+
+// //for12
+// let n = parseInt(prompt('n sonini kiriting: '))
+// let counter = 1
+
+// for(let i=1.1; i<=n;i+=0.1){
+//     counter*=i
+//     console.log(counter);
+// }
